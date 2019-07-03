5b59faff60a5a08ef1a9cf568b6764a3dc56fcab
TatianaKhokholko <Tatiana_Khokholko@epam.com>
2019-07-03 18:37:43 +0300

    module one

---
+++ Module1/ConsoleVariantApplications/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleVariantApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Командная строка содержит " + args.Length + " аргумента.\nВот они: ");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Hello " + args[i]);
+            }
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
