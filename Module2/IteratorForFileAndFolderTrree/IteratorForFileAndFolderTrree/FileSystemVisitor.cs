f459aadd67483ddbc77b9884d00e9170af2912f6
TatianaKhokholko <Tatiana_Khokholko@epam.com>
2019-07-12 10:27:10 +0300

    start write task 1

---
+++ Module2/IteratorForFileAndFolderTrree/IteratorForFileAndFolderTrree/FileSystemVisitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace IteratorForFileAndFolderTrree
+{
+    public class FileSystemVisitor
+    {
+        private readonly string startDirectory;
+
+        public FileSystemVisitor(string startDirectory)
+        {
+            this.startDirectory = startDirectory;
+        }
+
+        public IEnumerable<string> CustomIterator()
+        {
+            yield return "Каталоги: \n";
+
+            string[] directories = Directory.GetDirectories(startDirectory);
+            foreach (string dir in directories)
+            {
+                yield return dir;
+            }
+
+            yield return "Файлы: \n";
+
+            string[] files = Directory.GetFiles(startDirectory);
+            foreach (string file in files)
+            {
+                yield return file;
+            }
+        }
+    }
+}
---
+++ Module2/IteratorForFileAndFolderTrree/IteratorForFileAndFolderTrree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace IteratorForFileAndFolderTrree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите интересующий вас каталог (пример D:\\\\)\n");
+            string startDirectory = Console.ReadLine();
+            if (Directory.Exists(startDirectory))
+            {
+                FileSystemVisitor fileSystemVisitor = new FileSystemVisitor(startDirectory);
+
+                foreach (var item in fileSystemVisitor.CustomIterator())
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Такой каталог не существует!");
+            }
+
+            Console.WriteLine("Нажмите ENTER для выхода");
+            Console.ReadLine();
+        }
+    }
+}
