2b2495b7a4ac0744ad4a8e69ac8f2d4db9596c91
Tatiana Belka <TatianaBelkaRZN@gmail.com>
2019-11-23 15:12:36 +0300

    added module 6

---
+++ Module6/MyLoC/MyLoC/ExampleClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLoC
+{
+    class ExampleClass1
+    {
+        private void ExampleMethod()
+        {
+            Console.WriteLine("Only text output.");
+        }
+    }
+}
---
+++ Module6/MyLoC/MyLoC/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyLoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reflection();
+        }
+
+        public static void Reflection()
+        {
+            Type[] typeList = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (Type type in typeList)
+            {
+                Console.WriteLine($"\nReturn information about class: {type.Name}");
+                Console.WriteLine("Return information about all methods in current build:");
+                MethodInfo[] methodInfoMeta = type.GetMethods(BindingFlags.Instance
+                    | BindingFlags.Static
+                    | BindingFlags.Public);
+
+                foreach (var typeData in methodInfoMeta)
+                {
+                    Console.WriteLine(typeData.Name);
+                }
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
